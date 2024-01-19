797f62fe448899ea25456347a5f9a3db90f23cc0
SuttiGuy <Guy.guy0205245@gmail.com>
2024-01-19 16:52:37 +0700

    add_Chat

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import { useState, useEffect, useContext, useRef } from "react";
+import { UserContext } from "../context/UserContext";
+import axios from "axios";
+import Logo from "./Logo";
+import Contact from "./Contact";
 
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="flex h-screen">
+      <div className="bg-gray-200 w-1/3 flex flex-col">
+        <div className="flex-grow">
+          <Logo />
+          <Contact
+            username={"text"}
+            id={"65a78b71792ec13b0f439a42"}
+            online={true}
+            selected={true}
+          />
+          <Contact
+            username={"guy"}
+            id={"65a89fd992235af776fce553"}
+            online={false}
+            selected={false}
+          />
+          <Contact
+            username={"ssss"}
+            id={"65a78b71792ec13b4111111"}
+            online={false}
+            selected={false}
+          />
+        </div>
+        <div className="p-2 text-center flex items-center justify-center">
+          <span className="mr-2 text-sm text-gary-600 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-3 h-3"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-5.5-2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10 12a5.99 5.99 0 0 0-4.793 2.39A6.483 6.483 0 0 0 10 16.5a6.483 6.483 0 0 0 4.793-2.11A5.99 5.99 0 0 0 10 12Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Username
+          </span>
+          <button className="text-sm bg-blue-200 py-1 px-2 text-gray-500 border rounded-xl">
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+        <div className="flex-grow">
+          <div className="flex h-full flex-grow items-center justify-center">
+            <div className="text-gray-300">
+              &larr; Select a person from sidebar "_"
+            </div>
+          </div>
+        </div>
+        <form className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Type your message"
+            className="bg-white flex-grow border rounded-lg p-2"
+          />
+          <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-xl border-blue-200">
+            <input type="file" className="hidden" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15.621 4.379a3 3 0 0 0-4.242 0l-7 7a3 3 0 0 0 4.241 4.243h.001l.497-.5a.75.75 0 0 1 1.064 1.057l-.498.501-.002.002a4.5 4.5 0 0 1-6.364-6.364l7-7a4.5 4.5 0 0 1 6.368 6.36l-3.455 3.553A2.625 2.625 0 1 1 9.52 9.52l3.45-3.451a.75.75 0 1 1 1.061 1.06l-3.45 3.451a1.125 1.125 0 0 0 1.587 1.595l3.454-3.553a3 3 0 0 0 0-4.242Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+          <button
+            type="submit"
+            className="bg-blue-500 p2 text-white rounded-lg"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const User = require("./models/User");
+const Message = require("./models/Message");
+const ws = require("ws");
+const fs = require("fs");
 
 //เพื่อโหลดค่าตัวแปร
 dotenv.config();
@@ -16,7 +19,7 @@ app.use(cors({ credentials: true, origin: "CLIENT_URL" }));
 app.use(express.json());
 app.use(cookieParser());
 //set static(public) folder
-app.use("/uploads", express.static(__dirname + "/uploads"))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 //เชื่อมต่อฐานข้อมูล
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -25,22 +28,21 @@ app.get("/", (req, res) => {
   res.send("<h1>'This a ReSTful for MERN Chat'</h1>");
 });
 
-
 //Register
 const salt = bcrypt.genSaltSync(10);
 app.post("/register", async (req, res) => {
-    const { username, password } = req.body; 
-    try {
-      const userDoc = await User.create({
-        username,
-        password: bcrypt.hashSync(password, salt),
-      });
-      res.json(userDoc);
-    } catch (error) {
-      console.log(error);
-      res.status(400).json(error);
-    }
-  });
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
 
 //login
 const secret = process.env.SECRET;
@@ -50,43 +52,131 @@ app.post("/login", async (req, res) => {
   if (userDoc) {
     const isMatchedPassword = bcrypt.compareSync(password, userDoc.password);
     if (isMatchedPassword) {
-        //logged in
-    jwt.sign({ username, id: userDoc }, secret, {}, (err, token) => {
+      //logged in
+      jwt.sign({ username, id: userDoc }, secret, {}, (err, token) => {
         if (err) throw err;
         //Save data in cookie
         res.cookie("token", token).json({
           id: userDoc.id,
           username,
         });
-    });
-  }else {
-    res.status(400).json("wrong credentials");
-  }   
+      });
+    } else {
+      res.status(400).json("wrong credentials");
+    }
   } else {
     res.status(404).json("user not found");
   }
 });
 
 //User logout
-app.post("/logout", (req,res) =>{
-    res.cookie("token", "").json("ok");
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
-app.get("/profile", (req,res) =>{
-    const token = req.cookies?.token;
-    if(token) {
-        jwt.verify(token, secret, {}, (err,userData) =>{
-            if (err) throw err;
-            res.json(userData);
-        });
-    }else {
-        res.status(401).json("no token");
-    }
+app.get("/profile", (req, res) => {
+  const token = req.cookies?.token;
+  if (token) {
+    jwt.verify(token, secret, {}, (err, userData) => {
+      if (err) throw err;
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json("no token");
+  }
 });
-
 
 //Run Server
 const POST = process.env.POST;
-app.listen(POST, () => {
+const server = app.listen(POST, () => {
   console.log("Server is running on http://localhost:" + POST);
+});
+
+//web Socket Server
+const wss = new ws.WebSocketServer({ server });
+
+wss.on('connection', (connection, req) => {
+  const notifyAboutOnlinePeople = () => {
+    [...wss.clients].forEach((client) => {
+      client.send(
+        JSON.stringify({
+          online: [...wss.clients].map((c) => ({
+            userId: c.userId,
+            username: c.username,
+          })),
+        })
+      );
+    });
+  };
+  connection.timer = setInterval(() => {
+    connection.ping();
+    connection.deadTimer = setTimeout(() => {
+      connection.isAlive = false
+      clearInterval(connection.timer);
+      connection.terminate();
+      notifyAboutOnlinePeople();
+      console.log('dead');
+    }, 1000);
+  }, 5000);
+  connection.on('pong', () => {
+    clearTimeout(connection.deadTimer);
+  });
+
+  connection.on("pong", () => {
+    clearTimeout(connection.deadTimer);
+  });
+  //read username and id from the cookie
+  const cookie = req.headers.cookie;
+  if (cookie) {
+    //token=jdfsjafhuewqdsfsa;
+    //user=sfdgdsgfdgdfgdgdsgdg;
+    //date=123454
+    const tokenCookieString = cookie
+      .split(";")
+      .find((str) => str.startsWith("token="));
+    if (tokenCookieString) {
+      const token = tokenCookieString.split("=")(1);
+      if (token) {
+        jwt.verify(token, secret, {}, (err, userData) => {
+          if (err) throw err;
+          const { userId, username } = userData;
+          connection.userId = userId;
+          connection.username = username;
+        });
+      }
+    }
+  }
+  connection.on("message", async (message) => {
+    const messageData = JSON.parse(message.toString());
+    const { recipient, sender, text, file } = messageData;
+    let filename = null;
+    if (file) {
+      const parts = file.name.split(".");
+      const ext = parts[parts.length - 1];
+      filename = Date.now() + "." + ext;
+      const  path = __dirname + "/uploads/" +filename;
+      const bufferData = Buffer.from(file.data.split(",")[1], 'base64')
+      //อยู่ใน uploads
+      fs.writeFile(path,bufferData, ()=> {
+        console.log("file saved: " + path);
+      });
+    }
+    if(recipient && (text || file )){
+        const messageDoc = await Message.create({
+            sender: connection.userId,
+            recipient,
+            text,
+            file: file ? filename : null,    
+        }); 
+        [...wss.clients].filter(c=>c.userId === recipient).forEa(c=> c.send(JSON.stringify({
+            text,
+            file: file? filename : null,
+            sender: connection.userId,
+            recipient,
+            _id:messageData._id,
+        })))
+    }
+   
+  });
+  notifyAboutOnlinePeople();
 });
